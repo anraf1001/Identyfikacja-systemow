aa40c8b975749f187232ccd8d2f49f7b1b8bb63a
Andrzej Rafalski <anraf1001@gmail.com>
2022-05-16 08:59:26 +0200

    Update gitattributes

--- Lab3/zad_3_1.m
+++ Lab3/zad_3_1.m
@@ -11,13 +11,13 @@ y = DaneDynW(n, 2);
 N = length(u);
 
 %% Podział danych pomiarowych na 2 podzbiory
-u_est = u(1:fix(N/2));
-u_wer = u(fix(N/2)+1:end);
+N_est = fix(N/2);
+u_est = u(1:N_est);
+u_wer = u(N_est+1:end);
 
-y_est = y(1:fix(N/2));
-y_wer = y(fix(N/2)+1:end);
+y_est = y(1:N_est);
+y_wer = y(N_est+1:end);
 
-N_est = length(u_est);
 N_wer = length(u_wer);
 d = 2;
 
@@ -39,3 +39,18 @@ Phi = [yF, uF];
 pLS = pinv(Phi) * ypF;
 T0 = -1/pLS(1);
 k0 = T0 * pLS(2);
+
+%% Wykresy
+G0 = tf(2.0, [0.5, 1]);
+Gm = tf(k0, [T0, 1]);
+t_wer = (0:N_wer-1) * Tp;
+
+y0 = lsim(G0, u_wer, t_wer);
+ym = lsim(Gm, u_wer, t_wer);
+
+figure
+hold on
+plot(t_wer, y0)
+plot(t_wer, ym)
+legend('odpowiedź niezakłócona systemu y_0(n)', 'odpowiedź modelu symulowanego y_m(n)')
+grid on
